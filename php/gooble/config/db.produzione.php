6915e6b6d4998e0550e1bd1a49d59c24ce0d8255
unknown <giovanni.ragno@gmail.com>
2017-05-22 18:35:17 +0200

    rimosso file che non deve stare in locale

--- php/gooble/config/db.produzione.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=gooble',
-        'username' => 'gooble',
-        'password' => 'GoobleBike',
-    'charset' => 'utf8',
-];
